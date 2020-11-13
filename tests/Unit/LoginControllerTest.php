e097d6d95ab087a54320e51dea6b999fb6d433d3
Alessio <alessio.vertemati@gmail.com>
2020-11-13 10:40:01 +0100

    Login tests

--- tests/Unit/LoginControllerTest.php
+++ tests/Unit/LoginControllerTest.php
@@ -2,13 +2,19 @@
 
 namespace Tests\Unit;
 
+use Mockery;
 use Tests\TestCase;
+use SocialiteProviders\GitLab\Provider;
+use Tests\Fixtures\Concern\UseTestFixtures;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Validation\ValidationException;
+use SocialiteProviders\Manager\OAuth2\User as OauthUser;
 use Oneofftech\Identities\Facades\Identity as IdentityFacade;
+use Oneofftech\Identities\Facades\IdentityCrypt;
 
 class LoginControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, UseTestFixtures;
 
     public function test_redirect_to_provider()
     {
@@ -23,5 +29,87 @@ class LoginControllerTest extends TestCase
 
         $this->assertStringContainsString('gitlab.com', $location);
         $this->assertStringContainsString(route('oneofftech::login.callback', ['provider' => 'gitlab']), $location);
+    }
+
+    public function test_user_login()
+    {
+        $this->useTestFixtures();
+
+        $user = $this->createUser();
+
+        $identity = $user->identities()->create([
+            'provider'=> 'gitlab',
+            'provider_id'=> IdentityCrypt::hash('U1'),
+            'token'=> 'T1',
+            'refresh_token'=> null,
+            'expires_at'=> null,
+            'registration' => true,
+        ]);
+
+        $this->withoutExceptionHandling();
+
+        $driverMock = Mockery::mock(Provider::class)->makePartial();
+
+        $oauthFakeUser = (new OauthUser())->map([
+            'id'       => 'U1',
+            'nickname' => 'User',
+            'name'     => 'User',
+            'email'    => 'user@local.com',
+            'avatar'   => 'https://gitlab.com',
+            'token'   => 'T1',
+        ]);
+        
+        $driverMock->shouldReceive('user')->andReturn($oauthFakeUser);
+
+        $driverMock->shouldReceive('redirectUrl')->andReturn($driverMock);
+
+        IdentityFacade::shouldReceive('driver')->with('gitlab')->andReturn($driverMock);
+
+        $response = $this->get(route('oneofftech::login.callback', ['provider' => 'gitlab']));
+
+        $response->assertRedirect('http://localhost/home');
+
+        $this->assertAuthenticatedAs($user);
+    }
+    
+    public function test_user_login_denied_if_identity_cannot_be_found()
+    {
+        $this->useTestFixtures();
+
+        $user = $this->createUser();
+
+        $identity = $user->identities()->create([
+            'provider'=> 'facebook',
+            'provider_id'=> IdentityCrypt::hash('U2'),
+            'token'=> 'T1',
+            'refresh_token'=> null,
+            'expires_at'=> null,
+            'registration' => true,
+        ]);
+
+        $this->withoutExceptionHandling();
+
+        $driverMock = Mockery::mock(Provider::class)->makePartial();
+
+        $oauthFakeUser = (new OauthUser())->map([
+            'id'       => 'U1',
+            'nickname' => 'User',
+            'name'     => 'User',
+            'email'    => 'user@local.com',
+            'avatar'   => 'https://gitlab.com',
+            'token'   => 'T1',
+        ]);
+        
+        $driverMock->shouldReceive('user')->andReturn($oauthFakeUser);
+
+        $driverMock->shouldReceive('redirectUrl')->andReturn($driverMock);
+
+        IdentityFacade::shouldReceive('driver')->with('gitlab')->andReturn($driverMock);
+
+        $this->expectException(ValidationException::class);
+
+        $response = $this->get(route('oneofftech::login.callback', ['provider' => 'gitlab']));
+
+        $this->assertGuest();
     }
 }
